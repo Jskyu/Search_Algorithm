6bc202667047e8b0923103ef95a438a7bc2485fa
o3ojsk <hisunggyu@gmail.com>
2021-04-13 14:44:24 +0900

    init

---
+++ src/test/java/school_project/search_algorithm/SearchAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package school_project.search_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
